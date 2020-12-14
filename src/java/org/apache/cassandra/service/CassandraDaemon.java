814f2ac7e33aa81f6d056fc6b432c1bce0458d3e
Benjamin Lerer <b.lerer@gmail.com>
2020-12-14 13:27:37 +0100

    Reduce nesting in CassandraDaemon.migrateSystemDataIfNeeded

--- src/java/org/apache/cassandra/service/CassandraDaemon.java
+++ src/java/org/apache/cassandra/service/CassandraDaemon.java
@@ -504,64 +504,63 @@ public class CassandraDaemon
 
         // If there is only one directory and no system keyspace directory has been specified we do not need to do
         // anything. If it is not the case we want to try to migrate the data.
-        if (DatabaseDescriptor.useSpecificLocationForSystemData()
-                || DatabaseDescriptor.getNonSystemKeyspacesDataFileLocations().length > 1
-                || importSystemDataFrom != null)
+        if (!DatabaseDescriptor.useSpecificLocationForSystemData()
+                && DatabaseDescriptor.getNonSystemKeyspacesDataFileLocations().length <= 1
+                && importSystemDataFrom == null)
+            return;
+
+        // We can face several cases:
+        //  1) The system data are spread accross the data file locations and need to be moved to
+        //     the first data location (upgrade to 4.0)
+        //  2) The system data are spread accross the data file locations and need to be moved to
+        //     the system keyspace location configured by the user (upgrade to 4.0)
+        //  3) The system data are stored in the first data location and need to be moved to
+        //     the system keyspace location configured by the user (system_data_file_directory has been configured)
+        //  4) The system data have been stored in the system keyspace location configured by the user
+        //     and need to be moved to the first data location (the import of the data has been requested)
+        Path target = Paths.get(DatabaseDescriptor.getSystemKeyspacesDataFileLocations()[0]);
+
+        String[] nonSystemKeyspacesFileLocations = DatabaseDescriptor.getNonSystemKeyspacesDataFileLocations();
+        String[] sources = importSystemDataFrom != null
+            ? new String[] {importSystemDataFrom}
+            : DatabaseDescriptor.useSpecificLocationForSystemData() ? nonSystemKeyspacesFileLocations
+                                                                    : Arrays.copyOfRange(nonSystemKeyspacesFileLocations, 1, nonSystemKeyspacesFileLocations.length);
+
+        for (String source : sources)
         {
-            // We can face several cases:
-            //  1) The system data are spread accross the data file locations and need to be moved to
-            //     the first data location (upgrade to 4.0)
-            //  2) The system data are spread accross the data file locations and need to be moved to
-            //     the system keyspace location configured by the user (upgrade to 4.0)
-            //  3) The system data are stored in the first data location and need to be moved to
-            //     the system keyspace location configured by the user (system_data_file_directory has been configured)
-            //  4) The system data have been stored in the system keyspace location configured by the user
-            //     and need to be moved to the first data location (the import of the data has been requested)
-            Path target = Paths.get(DatabaseDescriptor.getSystemKeyspacesDataFileLocations()[0]);
+            Path dataFileLocation = Paths.get(source);
 
-            String[] nonSystemKeyspacesFileLocations = DatabaseDescriptor.getNonSystemKeyspacesDataFileLocations();
-            String[] sources = importSystemDataFrom != null
-                    ? new String[] {importSystemDataFrom}
-                    : DatabaseDescriptor.useSpecificLocationForSystemData() ? nonSystemKeyspacesFileLocations
-                                                                            : Arrays.copyOfRange(nonSystemKeyspacesFileLocations, 1, nonSystemKeyspacesFileLocations.length);
+            if (!Files.exists(dataFileLocation))
+                continue;
 
-
-            for (String source : sources)
+            try (Stream<Path> locationChildren = Files.list(dataFileLocation))
             {
-                Path dataFileLocation = Paths.get(source);
+                Path[] keyspaceDirectories = locationChildren.filter(p -> SchemaConstants.isLocalSystemKeyspace(p.getFileName().toString()))
+                        .toArray(Path[]::new);
 
-                if (!Files.exists(dataFileLocation))
-                    continue;
-
-                try (Stream<Path> locationChildren = Files.list(dataFileLocation))
+                for (Path keyspaceDirectory : keyspaceDirectories)
                 {
-                    Path[] keyspaceDirectories = locationChildren.filter(p -> SchemaConstants.isLocalSystemKeyspace(p.getFileName().toString()))
-                                                                 .toArray(Path[]::new);
-
-                    for (Path keyspaceDirectory : keyspaceDirectories)
+                    try (Stream<Path> keyspaceChildren = Files.list(keyspaceDirectory))
                     {
-                        try (Stream<Path> keyspaceChildren = Files.list(keyspaceDirectory))
+                        Path[] tableDirectories = keyspaceChildren.filter(Files::isDirectory)
+                                .filter(p -> !p.getFileName()
+                                        .toString()
+                                        .startsWith(SystemKeyspace.PAXOS))
+                                .toArray(Path[]::new);
+
+                        for (Path tableDirectory : tableDirectories)
                         {
-                            Path[] tableDirectories = keyspaceChildren.filter(Files::isDirectory)
-                                                                      .filter(p -> !p.getFileName()
-                                                                                     .toString()
-                                                                                     .startsWith(SystemKeyspace.PAXOS))
-                                                                      .toArray(Path[]::new);
+                            FileUtils.moveRecursively(tableDirectory,
+                                                      target.resolve(dataFileLocation.relativize(tableDirectory)));
+                        }
 
-                            for (Path tableDirectory : tableDirectories)
-                            {
-                                FileUtils.moveRecursively(tableDirectory,
-                                                          target.resolve(dataFileLocation.relativize(tableDirectory)));
-                            }
-
-                            if (!SchemaConstants.SYSTEM_KEYSPACE_NAME.equals(keyspaceDirectory.getFileName().toString()))
-                            {
-                                FileUtils.deleteDirectoryIfEmpty(keyspaceDirectory);
-                            }
+                        if (!SchemaConstants.SYSTEM_KEYSPACE_NAME.equals(keyspaceDirectory.getFileName().toString()))
+                        {
+                            FileUtils.deleteDirectoryIfEmpty(keyspaceDirectory);
                         }
                     }
                 }
-             }
+            }
         }
     }
 
