65cd829bc73e1a9232e11c0bcfbeaccee2536c53
Andrés de la Peña <a.penya.garcia@gmail.com>
2021-05-12 16:34:00 +0100

    Suggestions

--- test/unit/org/apache/cassandra/RepeatableRunner.java
+++ test/unit/org/apache/cassandra/RepeatableRunner.java
@@ -17,8 +17,9 @@
  */
 package org.apache.cassandra;
 
+import org.junit.runner.Description;
+import org.junit.runner.Runner;
 import org.junit.runner.notification.RunNotifier;
-import org.junit.runners.BlockJUnit4ClassRunner;
 import org.junit.runners.model.InitializationError;
 
 /**
@@ -29,19 +30,74 @@ import org.junit.runners.model.InitializationError;
  * If your test uses a runner already, as they can't be chained, you can use this class as inspiration to modify the
  * original runner to loop your test.
  */
-public class RepeatableRunner extends BlockJUnit4ClassRunner
+public class RepeatableRunner extends Runner
 {
+    private static final int REPETITIONS = Integer.parseInt(System.getProperty("cassandra.test.repetitions", "10"));
+
+    private final Runner runner;
+
+    private RepeatableRunner(Runner runner)
+    {
+        this.runner = runner;
+    }
+
     public RepeatableRunner(Class<?> klass) throws InitializationError
     {
-        super(klass);
+        this(new org.junit.runners.BlockJUnit4ClassRunner(klass));
+    }
+
+    @Override
+    public Description getDescription()
+    {
+        return runner.getDescription();
     }
 
     @Override
     public void run(RunNotifier notifier)
     {
-        for (int i = 0; i < 10; i++)
+        for (int i = 0; i < REPETITIONS; i++)
         {
-            super.run(notifier);
+            runner.run(notifier);
+        }
+    }
+
+    public static class BlockJUnit4ClassRunner extends RepeatableRunner
+    {
+        public BlockJUnit4ClassRunner(Class<?> klass) throws InitializationError
+        {
+            super(new org.junit.runners.BlockJUnit4ClassRunner(klass));
+        }
+    }
+
+    public static class Parameterized extends RepeatableRunner
+    {
+        public Parameterized(Class<?> klass) throws Throwable
+        {
+            super(new org.junit.runners.Parameterized(klass));
+        }
+    }
+
+    public static class BMUnitRunner extends RepeatableRunner
+    {
+        public BMUnitRunner(Class<?> klass) throws Throwable
+        {
+            super(new org.jboss.byteman.contrib.bmunit.BMUnitRunner(klass));
+        }
+    }
+
+    public static class OrderedJUnit4ClassRunner extends RepeatableRunner
+    {
+        public OrderedJUnit4ClassRunner(Class<?> klass) throws Throwable
+        {
+            super(new org.apache.cassandra.OrderedJUnit4ClassRunner(klass));
+        }
+    }
+
+    public static class CassandraIsolatedJunit4ClassRunner extends RepeatableRunner
+    {
+        public CassandraIsolatedJunit4ClassRunner(Class<?> klass) throws Throwable
+        {
+            super(new org.apache.cassandra.CassandraIsolatedJunit4ClassRunner(klass));
         }
     }
 }
