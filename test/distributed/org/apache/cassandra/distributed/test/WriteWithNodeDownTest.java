38ee7491c492aa046a352c0fbb78e4580110fa12
Andrés de la Peña <a.penya.garcia@gmail.com>
2021-08-06 12:10:41 +0100

    Wait after shutdown

--- test/distributed/org/apache/cassandra/distributed/test/WriteWithNodeDownTest.java
+++ test/distributed/org/apache/cassandra/distributed/test/WriteWithNodeDownTest.java
@@ -45,6 +45,7 @@ public class WriteWithNodeDownTest extends TestBaseImpl
             cluster.schemaChange(withKeyspace("CREATE TABLE %s.t (k int PRIMARY KEY, v int)"));
 
             cluster.get(2).shutdown().get();
+            Thread.sleep(5000);
 
             for (int i = 0; i < NUM_ROWS; i++)
                 cluster.coordinator(1).execute(withKeyspace("INSERT INTO %s.t (k, v) VALUES (?, ?)"), ONE, i, i);
