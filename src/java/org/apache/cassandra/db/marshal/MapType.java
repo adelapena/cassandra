9884406ae8644cdf8c57132b1ed41de84b1ff568
Andrés de la Peña <a.penya.garcia@gmail.com>
2022-08-19 16:33:25 +0100

    Add dynamic data masking functions

--- src/java/org/apache/cassandra/db/marshal/MapType.java
+++ src/java/org/apache/cassandra/db/marshal/MapType.java
@@ -20,6 +20,7 @@ package org.apache.cassandra.db.marshal;
 import java.nio.ByteBuffer;
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.Collections;
 import java.util.HashMap;
 import java.util.Iterator;
 import java.util.List;
