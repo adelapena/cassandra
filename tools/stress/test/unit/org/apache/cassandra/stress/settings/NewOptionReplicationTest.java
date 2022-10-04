332269614d805e517002f05cd36a6f4378db12fb
Andrés de la Peña <a.penya.garcia@gmail.com>
2022-10-04 16:23:49 +0100

    DO NOT MERGE - Random test changes

--- tools/stress/test/unit/org/apache/cassandra/stress/settings/NewOptionReplicationTest.java
+++ tools/stress/test/unit/org/apache/cassandra/stress/settings/NewOptionReplicationTest.java
@@ -21,10 +21,10 @@ package org.apache.cassandra.stress.settings;
 import com.google.common.collect.ImmutableMap;
 import org.junit.Test;
 
-import static org.junit.Assert.*;
+import static org.junit.Assert.assertEquals;
 
 // modified
-public class OptionReplicationTest
+public class NewOptionReplicationTest
 {
     @Test
     public void defaultsToReplicationFactorOfOne() throws Exception
