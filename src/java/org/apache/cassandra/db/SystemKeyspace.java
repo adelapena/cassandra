03f622f580f19b6eee2af060bac43debe9b3eb6b
Andrés de la Peña <a.penya.garcia@gmail.com>
2023-05-30 11:38:18 +0100

    Review feedback

--- src/java/org/apache/cassandra/db/SystemKeyspace.java
+++ src/java/org/apache/cassandra/db/SystemKeyspace.java
@@ -65,7 +65,6 @@ import org.apache.cassandra.dht.Token;
 import org.apache.cassandra.io.sstable.SSTableId;
 import org.apache.cassandra.io.sstable.SequenceBasedSSTableId;
 import org.apache.cassandra.io.util.DataOutputBuffer;
-import org.apache.cassandra.locator.IEndpointSnitch;
 import org.apache.cassandra.locator.InetAddressAndPort;
 import org.apache.cassandra.metrics.RestorableMeter;
 import org.apache.cassandra.net.MessagingService;
@@ -84,7 +83,6 @@ import org.apache.cassandra.schema.Views;
 import org.apache.cassandra.service.paxos.Commit;
 import org.apache.cassandra.service.paxos.PaxosState;
 import org.apache.cassandra.streaming.StreamOperation;
-import org.apache.cassandra.transport.ProtocolVersion;
 import org.apache.cassandra.utils.ByteBufferUtil;
 import org.apache.cassandra.utils.CassandraVersion;
 import org.apache.cassandra.utils.FBUtilities;
@@ -409,16 +407,6 @@ public final class SystemKeyspace
                          BuiltViews,
                          PreparedStatements,
                          Repairs);
-    }
-
-    private static volatile Map<TableId, Pair<CommitLogPosition, Long>> truncationRecords;
-
-    public enum BootstrapState
-    {
-        NEEDS_BOOTSTRAP,
-        COMPLETED,
-        IN_PROGRESS,
-        DECOMMISSIONED
     }
 
     public static void updateCompactionHistory(UUID id,
